8f926ee9a8b7af0f3cf0ac6d8d3077b9932ec0cf
Ashutosh Kumar <ashutoshkmr40@gmail.com>
2020-05-29 16:17:28 +0530

    common error component

---
+++ src/components/error/error.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const errorStyle = {
+  display: "flex",
+  height: "100%",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+export const ShowError = ({ errorMessage }) => {
+  return (
+    <div
+      // @ts-ignore
+      style={{ ...errorStyle }}
+    >
+      <Typography variant="overline">{errorMessage}</Typography>
+      <Typography variant="caption">
+        This may occur due to some browser extention
+      </Typography>
+    </div>
+  );
+};
